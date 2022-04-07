d6b55e62197aa716d3c88a54a40df8bc377ded1c
Raúl <raul.alonso.merino@gmail.com>
2022-04-07 21:59:50 +0200

    Adding anime trending route

--- routes/anime.routes.js
+++ routes/anime.routes.js
@@ -1,18 +1,21 @@
 const express = require("express");
-const axios = require("axios")
+const axios = require("axios");
 
 const router = express.Router();
 
 async function getTrendingAnime() {
-    const url = "https://kitsu.io/api/edge/trending/anime"
-    
-    const response = await axios.get(url)
-    console.log(response)
-  }
+  const url = "https://kitsu.io/api/edge/trending/anime";
+  const response = await axios.get(url);
+  return response.data.data;
+}
 
 router.get("/home", async (req, res) => {
-    /* const trend = await getTrendingAnime() */
-res.send("hello")
-})
+  try {
+    const trend = await getTrendingAnime();
+    res.status(200).json(trend);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
