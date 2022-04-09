ab03c25a2517a6eeed189dfece3e96be1e486e91
Gonçalo Estrelado <goncalo.estrelado@hotmail.com>
2022-04-09 16:14:36 +0100

    anime routes

--- routes/anime.routes.js
+++ routes/anime.routes.js
@@ -9,10 +9,26 @@ async function getTrendingAnime() {
   return response.data.data;
 }
 
+async function getAnimeData(id) {
+  const url = `https://kitsu.io/api/edge/anime/${id}`;
+  const response = await axios.get(url)
+  return response.data.data
+}
+
 router.get("/home", async (req, res) => {
   try {
     const trend = await getTrendingAnime();
     res.status(200).json(trend);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    console.log(req.params)
+    const item = await getAnimeData(req.params.id);
+    res.status(200).json(item);
   } catch (error) {
     res.status(500).json(error);
   }
