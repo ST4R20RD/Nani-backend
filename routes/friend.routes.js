b315421e55477b64f3ecf243e4b168f1b88846a9
Raúl <raul.alonso.merino@gmail.com>
2022-04-21 19:37:59 +0200

    added search friends function

--- routes/friend.routes.js
+++ routes/friend.routes.js
@@ -4,65 +4,73 @@ const User = require("../models/User.model");
 
 const router = express.Router();
 
+// get all people signed up
+router.get("/", authenticate, async (req, res) => {
+  const users = await User.find();
+  res.status(200).json(users);
+});
+
+// get specific user in the database with id
 router.get("/:id", authenticate, async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id).populate("following").populate("followers");
-      const following = user.following;
-      const followers = user.followers;
-      res.status(200).json({following,followers});
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
-  
-  router.get(
-    "/search/:searchBarInput",
-    authenticate,
-    async (req, res) => {
-      try {
-        const { searchBarInput } = req.params;
-        const result = await User.find({ username: `${searchBarInput}` });
-        res.status(200).json(result);
-      } catch (error) {
-        res.status(500).json(error);
-      }
-    }
-  );
-  
-  function isUserEqualTo(object1, object2) {
-    if (object1.length === 0) return false;
-    for (let i = 0; i <= object1.length; i++) {
-      if (object1[i]._id.equals(object2._id)) {
-        return true;
-      } else {
-        return false;
-      }
+  try {
+    const user = await User.findById(req.params.id)
+      .populate("following")
+      .populate("followers");
+    const following = user.following;
+    const followers = user.followers;
+    res.status(200).json({ following, followers });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// get searched user in the database with mathing query for username
+router.get("/search/:searchBarInput", authenticate, async (req, res) => {
+  try {
+    const { searchBarInput } = req.params;
+    const result = await User.find({
+      username: { $regex: `${searchBarInput}`, $options: "i" },
+    });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+function isUserEqualTo(object1, object2) {
+  if (object1.length === 0) return false;
+  for (let i = 0; i <= object1.length; i++) {
+    if (object1[i]._id.equals(object2._id)) {
+      return true;
+    } else {
+      return false;
     }
   }
-  
-  router.get("/:id/add", authenticate, async (req, res) => {
-    try {
-      const user = await User.findById(req.jwtPayload.user._id);
-      const friend = await User.findById(req.params.id);
-      if (!isUserEqualTo(user.following, friend)) {
-        user.following.push(friend._id);
-        friend.followers.push(user._id);
-      }
-      user.save();
-      friend.save();
-      res.status(200).json(user);
-    } catch (error) {
-      res.status(500).json(error);
+}
+
+router.get("/:id/add", authenticate, async (req, res) => {
+  try {
+    const user = await User.findById(req.jwtPayload.user._id);
+    const friend = await User.findById(req.params.id);
+    if (!isUserEqualTo(user.following, friend)) {
+      user.following.push(friend._id);
+      friend.followers.push(user._id);
     }
-  });
-  
-  router.get("/friendProfile/:id", authenticate, async (req, res) => {
-    try {
-      const friend = await User.findById(req.params.id);
-      res.status(200).json(friend);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  });
+    user.save();
+    friend.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/friendProfile/:id", authenticate, async (req, res) => {
+  try {
+    const friend = await User.findById(req.params.id);
+    res.status(200).json(friend);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
