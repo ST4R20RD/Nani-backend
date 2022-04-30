34aa8dded36b96232f4db380453eca866398ed7d
Raúl <raul.alonso.merino@gmail.com>
2022-04-30 17:19:57 +0200

    heroku

--- server.js
+++ server.js
@@ -42,13 +42,17 @@ app.use("/email", emailRoutes);
 
 app.listen(process.env.PORT);
 
-const io = new Server({
+/* const io = new Server({
   cors: {
     origin: `http://localhost:3000`,
   },
-});
+}); */
 
-/* const io = new Server(cors()); */
+const io = new Server({
+  cors: {
+    origin: `https://nani-app.netlify.app`,
+  },
+});
 
 let onlineUsers = [];
 
