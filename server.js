5fdf5af642e86f85b5c2aeaf5200937bf6ba84c7
Raúl <raul.alonso.merino@gmail.com>
2022-05-06 18:05:45 +0200

    Deploying heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(passport.initialize());
+/* app.use(passport.initialize()); */
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
